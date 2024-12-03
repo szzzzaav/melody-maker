90b7671daa42ef63adb631bfe11b06c952187690
szzzzaav <sulaimuzzz@sina.com>
2024-12-03 14:41:31 +0800

    e

--- src/types/instruments.ts
+++ src/types/instruments.ts
@@ -1,1 +1,17 @@
- 
+export type Note = string;
+export type Octave = string;
+
+export interface InstrumentConfig {
+  notes: Note[];
+  octaves: Octave[];
+}
+
+export interface InstrumentRanges {
+  [note: string]: Octave[];
+}
+
+export type InstrumentName =
+  | "piano"
+  | "guitar"
+  | "bass"
+  | "drums";
